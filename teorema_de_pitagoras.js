dfa8ed7c9410e3b4ba3982b6e1c17fcf5f56c6ac
Gonzalez-Adan <gonzalezadan486@gmail.com>
2024-01-17 19:31:03 -0300

    Probando

--- teorema_de_pitagoras.js
+++ teorema_de_pitagoras.js
@@ -1,5 +1,4 @@
 let a = prompt("Ingresa el valor de a");
 let b = prompt("Ingresa el valor de b");
 let hipo = Math.sqrt(Math.pow(a,2) + Math.pow(b,2));
-
 console.log(`La raiz cuadrada es: ${hipo}`);
